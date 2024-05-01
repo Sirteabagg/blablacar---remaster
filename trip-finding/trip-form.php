c9258abe28a54fa0e6062e7bf8b02bd9bc10577a
ethanECE <ethan.petain@edu.ece.fr>
2024-05-01 19:51:30 +0200

    ajout font et titre du site

--- trip-finding/trip-form.php
+++ trip-finding/trip-form.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style-main-structure.css">
+    <link rel="stylesheet" href="style-trip-finding.css">
     <title>Document</title>
 </head>
 
 <body>
     <main>
-
+        <div class="title">
+            <h1>BlaBla OMNES</h1>
+        </div>
     </main>
     <footer>
         <nav>
