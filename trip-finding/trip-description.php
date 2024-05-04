4d4f69e787bb15fd03365b56cb6d4e263bfe0089
ethanECE <ethan.petain@edu.ece.fr>
2024-05-04 18:58:21 +0200

    ajout de la stucture de la selection des trajets

---
+++ trip-finding/trip-description.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style-main-structure.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="title-description">
+            <a href="trip-selection.php">
+                <div class="retour">&lt;</div>
+            </a>
+        </div>
+    </header>
+</body>
+
+</html>
