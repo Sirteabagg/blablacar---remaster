51411c15eafceb7e2176c4d5baa932aca11f775b
prige <thomas.prigent@edu.ece.fr>
2024-05-05 17:43:09 +0200

    bases profil

--- profil/profilforme.php
+++ profil/profilforme.php
@@ -1,1 +1,13 @@
-<h1>Thomas test2</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style-main-structure.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Mon profil</h1>
+    
+</body>
+</html>
