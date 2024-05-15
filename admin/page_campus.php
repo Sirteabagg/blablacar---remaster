b09f7a5227d6a1633fe8ed51ed0059eed539db51
hugoriviere <hugo.riviere@edu.ece.fr>
2024-05-15 17:46:11 +0200

    creation page campus

---
+++ admin/page_campus.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style-main-structure.css">
+    <link rel="stylesheet" href="styles/style_admin.css">
+   
+
+    <title>Document</title>
+</head>
+
+<body class="page-type">
+
+<div class="partiedroite droite">
+        <div class=" titre">Campus</div> 
+        <div class=" titre">Liste Utilisateur</div>
+        <div class=" titre">Gestion de permis</div>
+        
+                    
+  </div>
+
+    
+  <div class="partiegauche gauche">
+        <div class="titre22 titre">Campus</div>
+    
+        <div class="truc1">1111</div> 
+     
+        <div class="truc2">2222</div> 
+  
+        <div class="bouton">OOOO</div> 
+      
+        
+                    
+  </div>
+ 
+   
+  
+
+</body>
+
+
+</html>
