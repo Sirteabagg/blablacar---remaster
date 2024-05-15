6653679f324bc17bf65c09eb77a2f0f6ad16cbaf
PRIGENT Thomas <prigentthomas.2004@gmail.com>
2024-05-15 17:59:06 +0200

    creatpageT

--- profil/php/connexion.php
+++ profil/php/connexion.php
@@ -35,7 +35,7 @@ if (strpos($systeme_exploitation, 'Darwin') !== false) {
         <input type="password" placeholder="Mot de passe" name="mdp" class="form-input">
         <input type="submit" class="button-submit" value="Se connecter">
     </form>
-    <input type="submit" class="button-submit" value="Créer un compte">
+    <a href="create.php"><input type="submit" class="button-submit" value="Créer un compte"></a>
     <div class="img">
         <img src="../../images/logoblabla.png" width="275" height="320">
     </div>
--- profil/php/create.php
+++ profil/php/create.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../css/style-main-structure.css">
+    <link rel="stylesheet" href="../styles/create.css">
 </head>
 <body>
-    
+    <h1 class="titre">Création</h1>
+    <form action="verif-connexion.php" method="post">
+        <input type="text" placeholder="Email" name="email" class="form-input">
+        <input type="password" placeholder="Mot de passe" name="mdp" class="form-input">
+        <input type="submit" class="button-submit" value="Créer">
+    </form>
 </body>
 </html>
