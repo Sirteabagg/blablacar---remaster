890914b6a7f208a963bcc2d9cd3fca7cb84e577b
ethanECE <ethan.petain@edu.ece.fr>
2024-05-21 19:32:51 +0200

    rien a declarer

--- trip-finding/php/trip-ressources/trip-selection.php
+++ trip-finding/php/trip-ressources/trip-selection.php
@@ -38,7 +38,7 @@ if (isset($_POST["departure"], $_POST["arrival"], $_POST["date"], $_POST["passen
 $depart = str_replace(' ', '+', $depart);
 $arrivee = str_replace(' ', '+', $arrivee);
 
-$url_api_adresse_dep = "https://api-adresse.data.gouv.fr/search/?q=$depart&limit=1";
+$url_api_adresse_dep = "https://api-adresse.data.gouv.fr/search/?q=Lyon&limit=1";
 $url_api_adresse_arr = "https://api-adresse.data.gouv.fr/search/?q=$arrivee&limit=1";
 
 // Initialisation de CURL
@@ -69,7 +69,15 @@ if (curl_errno($curl_dep) && curl_errno($curl_arr)) {
         // Extraire les coordonnées géographiques de la première entité (limit=1)
         $depcity = $donnees_dep["features"][0]["properties"]["city"];
         $arrcity = $donnees_arr["features"][0]["properties"]["city"];
-        echo $arrcity;
+        $geometry = $donnees_dep['features'][0]['geometry'];
+        $latitude_dep = $geometry['coordinates'][1]; // Latitude
+        $longitude_dep = $geometry['coordinates'][0]; // Longitude
+        $geometry = $donnees_arr['features'][0]['geometry'];
+        $latitude_arr = $geometry['coordinates'][1]; // Latitude
+        $longitude_arr = $geometry['coordinates'][0]; // Longitude
+
+        echo $response_dep;
+        echo $latitude_dep;
     } else {
         echo "Aucune adresse trouvée.";
     }
@@ -78,6 +86,7 @@ if (curl_errno($curl_dep) && curl_errno($curl_arr)) {
 // Fermer la session CURL
 curl_close($curl_dep);
 curl_close($curl_arr);
+
 ?>
 
 
@@ -108,81 +117,36 @@ curl_close($curl_arr);
         </div>
     </header>
     <main>
-        <a href="trip-description.php">
-            <div class="trip-container">
-                <div>
-                    <!-- code image dans test-bdd -->
-                    <img src="../../images/utilisateur.png" class="img-user">
-                </div>
-                <div>
-                    <span>Nom</span>
-                    <div>
-                        <div class="avis-container">
-                            <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="10px" height="10px" viewBox="0 0 1208.000000 1280.000000" preserveAspectRatio="xMidYMid meet">
-                                <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
-                                    <path d="M6036 12519 c-20 -58 -954 -3088 -1237 -4011 l-73 -238 -2123 0
-                                    c-1168 0 -2123 -3 -2122 -7 0 -5 23 -25 50 -46 28 -21 571 -438 1207 -926 636
-                                    -488 1380 -1057 1652 -1265 272 -208 501 -384 508 -390 10 -9 -123 -450 -643
-                                    -2140 -360 -1171 -654 -2130 -652 -2132 1 -1 36 23 77 54 99 74 2006 1534
-                                    2763 2115 l598 459 37 -28 c20 -15 237 -181 482 -369 363 -278 2650 -2030
-                                    2864 -2193 l59 -46 -7 30 c-4 16 -297 973 -652 2127 -355 1154 -643 2104 -641
-                                    2112 3 7 140 115 304 241 224 171 2139 1640 3036 2328 42 32 77 63 77 68 0 4
-                                    -954 8 -2120 8 l-2119 0 -45 143 c-25 78 -210 678 -411 1332 -606 1969 -857
-                                    2780 -861 2784 -2 2 -6 -2 -8 -10z" />
-                                </g>
-                            </svg>
-                            <span>5</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="price">0,--€</div>
-                <div class="trip-info">
-                    <div class="col" id="col1">
-                        <div class="dep center-item-col">
-                            départure
-                        </div>
-                        <!-- espace -->
-                        <div class="line-container">
-                            <div class="line-hor" id="line-color-white"></div>
-                            <div class="point center-item-col">
-                                <div class="circle-bg">
-                                    <div class="circle-upper"></div>
-                                </div>
-                            </div>
-                            <div class="line-hor" id="line-color-green"></div>
-                        </div>
-                        <div class="hour center-item-col">
-                            10h
-                        </div>
-                    </div>
-                    <div class="col-flex" id="col2">
-                        <div class="trait-horizontal"></div>
-                    </div>
-                    <div class="col" id="col3">
-                        <div class="arr center-item-col">
-                            arrivée
-                        </div>
-                        <div class="line-container">
-                            <div class="line-hor" id="line-color-green"></div>
-                            <div class="point center-item-col">
-                                <div class="circle-bg">
-                                    <div class="circle-upper"></div>
-                                </div>
-                            </div>
-                            <div class="line-hor" id="line-color-white"></div>
-                        </div>
-                        <div class="hour center-item-col">
-                            10h
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </a>
         <?php
         foreach ($trips as $trip) {
-            // faire la méthode GET dans le a href
+            // $url_api_adresse = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248734c41a8117a44f6839360a0e5bbe9f9&start=$longitude_dep,$latitude_dep&end=$longitude_arr,$latitude_arr";
+            // $curl = curl_init();
+
+            // curl_setopt($curl, CURLOPT_URL, $url_api_adresse);
+            // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+            // $response = curl_exec($curl);
+
+            // if (curl_errno($curl)) {
+            //     echo 'Erreur CURL : ' . curl_error($curl);
+            // } else {
+            //     // Décoder la réponse JSON en un tableau associatif PHP
+            //     $donnees = json_decode($response, true);
+            //     echo $response;
+            //     if (isset($donnees["features"]) && !empty($donnees["features"])) {
+            //         $distance = $donnees["features"][0]["properties"]["segments"][0]["distance"];
+            echo $distance;
+            //     }
+            // }
+
+            // curl_close($curl);
+
             echo '<a href="trip-description.php?idTrip=' . $trip["idTrip"] . '"><div class="trip-container">';
-            echo '<div><img src="../../images/utilisateur.png" class="img-user"></div>';
+            $contenu_image = $trip['pdp'];
+            $type_mime = 'image/jpeg'; // Remplacez par le type MIME de votre image si nécessaire
+            $encoded_image = base64_encode($contenu_image);
+            $image_data = "data:$type_mime;base64,$encoded_image";
+            echo "<img src=\"$image_data\" class='img-user' alt=\"Image\">";
             echo '<div><span>' . $trip['prenom'] . '</span>';
             echo '<div><div class="avis-container"><svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="10px" height="10px" viewBox="0 0 1208.000000 1280.000000" preserveAspectRatio="xMidYMid meet"><g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none"><path d="M6036 12519 c-20 -58 -954 -3088 -1237 -4011 l-73 -238 -2123 0 c-1168 0 -2123 -3 -2122 -7 0 -5 23 -25 50 -46 28 -21 571 -438 1207 -926 636 -488 1380 -1057 1652 -1265 272 -208 501 -384 508 -390 10 -9 -123 -450 -643 -2140 -360 -1171 -654 -2130 -652 -2132 1 -1 36 23 77 54 99 74 2006 1534 2763 2115 l598 459 37 -28 c20 -15 237 -181 482 -369 363 -278 2650 -2030 2864 -2193 l59 -46 -7 30 c-4 16 -297 973 -652 2127 -355 1154 -643 2104 -641 2112 3 7 140 115 304 241 224 171 2139 1640 3036 2328 42 32 77 63 77 68 0 4 -954 8 -2120 8 l-2119 0 -45 143 c-25 78 -210 678 -411 1332 -606 1969 -857 2780 -861 2784 -2 2 -6 -2 -8 -10z" /></g></svg><span>' . $trip["notegenerale"] . '</span></div></div></div>';
             echo '<div class="price">' . $trip['price'] . '€</div>';
