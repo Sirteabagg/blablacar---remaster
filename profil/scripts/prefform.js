65519408d5b7ca8d725d119b024b1abd01caa79b
PRIGENT Thomas <prigentthomas.2004@gmail.com>
2024-05-23 00:12:47 +0200

    prefThomas

--- profil/scripts/prefform.js
+++ profil/scripts/prefform.js
@@ -1,3 +1,21 @@
-function toggleExpand(element) {
-    element.classList.toggle('expanded');
-}
+$('.content').children().each(function(){
+    $(this).hide();
+});
+
+$('.content2').children().each(function(){
+    $(this).hide();
+});
+
+
+
+$('.fleche').click(function(){
+    $('.content').children().each(function(){
+        $(this).toggle();
+    })
+})
+
+$('.fleche2').click(function(){
+    $('.content2').children().each(function(){
+        $(this).toggle();
+    })
+})
