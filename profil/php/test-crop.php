1724f3d44fb0f9df7c1d20fa2f5b129187e34227
ethanECE <ethan.petain@edu.ece.fr>
2024-05-26 00:59:24 +0200

    ajout crop image

---
+++ profil/php/test-crop.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Cropper</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
+    <script src="../scripts/crop.js" defer></script>
+    <style>
+        img {
+            display: block;
+            max-width: 50%;
+        }
+
+        .preview {
+            overflow: hidden;
+            width: 160px;
+            height: 160px;
+            margin: 10px;
+            border: 1px solid black;
+        }
+    </style>
+</head>
+
+<body>
+    <input type="file" id="fileInput">
+    <div>
+        <img id="image" style="display:none;">
+    </div>
+    <button id="cropButton">Crop</button>
+    <div class="preview" id="preview"></div>
+
+</body>
+
+</html>
