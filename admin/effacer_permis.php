f19370076f4049f3f2ef1511b4680741f3008783
hugoriviere <hugo.riviere@edu.ece.fr>
2024-05-26 18:18:13 +0200

    information sur page Ethan

--- admin/effacer_permis.php
+++ admin/effacer_permis.php
@@ -36,6 +36,11 @@ if (isset($_POST["boutonpermis"])) {
         caption : <?php echo $donnees['caption']; ?><br>
       </p>
     <?php
+    $contenu_image = $donnees['pdp'];
+    $type_mime = 'image/jpeg'; // Remplacez par le type MIME de votre image si nécessaire
+    $encoded_image = base64_encode($contenu_image);
+    $image_data = "data:$type_mime;base64,$encoded_image";
+    echo "<img src=\"$image_data\" class='img-user' alt=\"Image\">";
     }
     ?>
     <form method="post" action="page_permis.php" class="listepermis ">
