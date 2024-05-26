d6bb8730985b28e2c9d597ff94e2825fe37ee5f7
ethanECE <ethan.petain@edu.ece.fr>
2024-05-26 15:26:19 +0200

    ajout condition driver

--- trip-finding/php/trip-ressources/mytrip-acceptation.php
+++ trip-finding/php/trip-ressources/mytrip-acceptation.php
@@ -5,27 +5,35 @@ session_start();
 
 
 $email = $_SESSION["current-user-email"];
-echo $email;
 
-$requestUser = $bdd->query("SELECT d.idDriver as driver FROM Driver d JOIN `User` u on d.email = u.email WHERE u.email = '$email'");
-$idDriver = $requestUser->fetch()["driver"];
-echo $idDriver;
-$requestidDriver = $bdd->query("SELECT * FROM Booking b WHERE b.idDriver = $idDriver");
+
+$checkUser = $bdd->query("SELECT COUNT(*) as here FROM Driver d JOIN `User` u on d.email = u.email WHERE u.email = '$email'");
+$emailHere = $checkUser->fetch()["here"];
+if ($emailHere == 0) {
+    echo "vous n'etes pas enregistrer comme conducteur";
+} else {
+    $requestUser = $bdd->query("SELECT d.idDriver as driver FROM Driver d JOIN `User` u on d.email = u.email WHERE u.email = '$email'");
+    $idDriver = $requestUser->fetch()["driver"];
+    $requestidDriver = $bdd->query("SELECT * FROM Booking b WHERE b.idDriver = $idDriver");
+
+
+
 
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../styles/accept-trip.css">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../styles/accept-trip.css">
+        <link rel="stylesheet" href="../../../css/style-main-structure.css">
+        <title>Document</title>
+    </head>
 
-<body>
+    <body>
     <?php
     while ($donnee = $requestidDriver->fetch()) {
         $idPassenger = $donnee["idPassenger"];
@@ -55,15 +63,15 @@ $requestidDriver = $bdd->query("SELECT * FROM Booking b WHERE b.idDriver = $idDr
             </div>';
         }
     }
-
+}
     ?>
 
 
 
-</body>
+    </body>
 
-</html>
+    </html>
 
-<!--quand user accepte envoie ajoute passed = 1 dans la table booking
+    <!--quand user accepte envoie ajoute passed = 1 dans la table booking
     et ajout l'idpassager dans la table car_passengers
     si refus mettre passed = 1 et ne pas ajouter l'idPassager -->
